da69d8d0d81381b4ea913c8881b93e9cd3ff7c4a
arsscriptum <guillaumeplante.qc@gmail.com>
2023-03-16 13:32:43 -0400

    auto

--- scripts/fix_apt.sh
+++ scripts/fix_apt.sh
@@ -19,13 +19,23 @@ setup_log "Fix Started"
 
 for i in $(dpkg -l|awk '/^ii/ {print $2}')
 do
-	setup_log "einstall $i"
+	setup_log "reinstall $i"
     sudo apt-get --reinstall -y install $i
 done
 
 setup_log "DONE"
 
-#dpkg -l | grep ii | awk '{print "apt-get --reinstall -y install", $2}' > /tmp/reinstall
-#sh /tmp/reinstall
+setup_log "Fix Started 2 " 
+for x in  $(dpkg -l| awk '/^ii  apt/ {print $2}'); 
+do 
+	setup_log "reinstall $x"
+	apt-get install --reinstall ${x} -y ; 
+done
+
+setup_log "Fix Started 3" 
+setup_log "outputing in $MYTMP/reinstall.sh"
+
+dpkg -l | grep ii | awk '{print "apt-get --reinstall -y install", $2}' > $MYTMP/reinstall.sh
+sh $MYTMP/reinstall.sh
 setup_log "apt-get --reinstall install ucf"
 apt-get --reinstall install ucf
